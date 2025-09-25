89d9cde88beac93861f32146623d0e0b4f4c8932
JavariThomas <javari702@gmail.com>
2025-09-25 18:28:46 +0200

    idk atp lol

--- MidtermProject/Assets/Scripts/Interactables/Spikes.cs
+++ MidtermProject/Assets/Scripts/Interactables/Spikes.cs
@@ -3,18 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Spikes : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
